4d65d3e6da3e728d2054eb5bef061e0071fd1189
Arne Kiesewetter <banane1996@gmx.net>
2014-08-18 21:28:36 +0200

    Fix not right format..

--- XmlRpc/Properties/AssemblyInfo.cs
+++ XmlRpc/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
